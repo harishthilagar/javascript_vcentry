97af6e5d7dfac5d8713d4d05c715e541612987d3
harithilagar <harishthilagar06@gmail.m>
2021-03-08 13:51:22 +0530

    class06

--- day6/sample5/task.js
+++ day6/sample5/task.js
@@ -9,6 +9,3 @@ console.log(arr);
 for(let i = arr.length-1;i >= 0;i--){
   console.log(arr[i]);
 }
-console.log("--------------------------------------");
-
-let arra = [][];
---
+++ day6/sample5/task1.js
@@ -0,0 +1,9 @@
+let arr = [];
+let k=10;
+for(let i = 0;i<33;i++){
+  for(let j = 0;j < 3;j++){
+    k = k + 1;
+     arr[j]= "vcentry"+k;
+  }
+  console.log(arr);
+}
